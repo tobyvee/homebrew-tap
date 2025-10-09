4f2f0ea73694654d040674d24ea8cb22a416621a
Toby V <github@vervaart.co>
2025-10-10 10:10:36 +1100

    Fix checksum

--- Formula/diskmute.rb
+++ Formula/diskmute.rb
@@ -2,7 +2,7 @@ class Diskmute < Formula
   desc "Turn off indexing and remove metadata on a volume mounted in MacOS"
   homepage "https://github.com/tobyvee/diskmute"
   url "https://github.com/tobyvee/diskmute/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "5d04961f5e1f54d9730d13757ded8418d7bc9a1fec394072d0908b8d8da99e3b"
+  sha256 "35b5d4457ba5d278aa230b969d6f6ec79f075811e2e0d9ced2966db9d6f023ba"
   version "v1.0.0"
 
   def install
