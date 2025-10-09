8ffcc0e627253decc1091bb04b35270dace30dd6
Toby V <github@vervaart.co>
2025-10-09 17:13:26 +1100

    Fix formula style issues and bump version

--- Formula/diskguard.rb
+++ Formula/diskguard.rb
@@ -2,9 +2,9 @@ class Diskguard < Formula
   desc "Software based USB write blocker for MacOS"
   homepage "https://github.com/tobyvee/diskguard"
   url "https://github.com/tobyvee/diskguard.git",
-      tag:      "v1.0.0",
-      revision: "5b7ce8d96edc64a8596e55da7eb3bfac3546a5ee"
-  version "1.0.0"
+      tag:      "v1.1.0",
+      revision: "6c183d97501680d33b2b09810d7d6a7d11e5922c"
+  version "1.1.0"
 
   def install
     bin.install "diskguard.sh" => "diskguard"
