47076bf432990c68086db03e9c599793145ee09e
Toby V <github@vervaart.co>
2025-10-12 12:58:36 +1100

    Fix typo

--- Formula/diskguard.rb
+++ Formula/diskguard.rb
@@ -1,7 +1,7 @@
 class Diskguard < Formula
   desc "Software based USB write blocker for MacOS"
   homepage "https://github.com/tobyvee/diskguard"
-  url "https://github.com/tobyvee/diskguard/archive/refs/tags/v1.1.0.tar.gz",
+  url "https://github.com/tobyvee/diskguard/archive/refs/tags/v1.1.0.tar.gz"
   sha256 "485ef0141a0f25a29e11e6fd4067996d7f49974c9cc17de661142ec636582350"
   version "v1.1.0"
 
